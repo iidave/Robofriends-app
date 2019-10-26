9c570a5de022220c2b0f7a31eb79f071a1d709aa
iidave <iidave@msn.com>
2019-10-25 23:42:33 -0700

    Add to repo

---
+++ build/precache-manifest.30ce0f84184882a9df5d1f891bd2fd31.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4426d5cd6001bc9317c1cff850fea9c3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2b4ae4f679bdf8a07ba8",
+    "url": "/static/css/2.31dafeb2.chunk.css"
+  },
+  {
+    "revision": "da31a9bfbae7f9d75502",
+    "url": "/static/css/main.9e83fe8e.chunk.css"
+  },
+  {
+    "revision": "2b4ae4f679bdf8a07ba8",
+    "url": "/static/js/2.f7eb401d.chunk.js"
+  },
+  {
+    "revision": "da31a9bfbae7f9d75502",
+    "url": "/static/js/main.e15c72f9.chunk.js"
+  },
+  {
+    "revision": "2fea5e571640de4687f7",
+    "url": "/static/js/runtime-main.83bf741c.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/static/media/SEGA.12a3db8b.woff"
+  }
+]);
